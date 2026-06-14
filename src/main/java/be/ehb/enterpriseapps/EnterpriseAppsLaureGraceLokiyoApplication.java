46ea32605d90aeeabfab8e1fbe27f59654a88602
Laure Grace Lokiyo <lokiyolauregrace@gmail.com>
2026-06-14 20:27:23 +0200

    Initial Spring Boot project

---
+++ src/main/java/be/ehb/enterpriseapps/EnterpriseAppsLaureGraceLokiyoApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppsLaureGraceLokiyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAppsLaureGraceLokiyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/enterpriseapps/EnterpriseAppsLaureGraceLokiyoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppsLaureGraceLokiyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
